1665a80dab35f9be862d346052282f8fd0a02bdb
Janeil Alcano <janeilalcano01@gmail.com>
2020-04-09 21:53:20 +0800

    Updated design

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
 
+window.onload = function () {
+  displayStores();
+
+}
+
+var map;
+var markers = [];
+var infoWindow;
+
 
 function initMap() {
-  var philippines = {
+  var losAngeles = {
+    lat: 34.063380,
+    lng: -118.358080,
+  };
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: losAngeles,
     zoom: 11,
-    center: new google.maps.LatLng(14.599512, 120.984222),
     mapTypeId: 'roadmap',
     styles: [
       { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
@@ -85,15 +98,89 @@ function initMap() {
         stylers: [{ color: '#17263c' }]
       }
     ]
-  };
-  var map = new google.maps.Map(document.getElementById('map'), philippines);
-
-  var manilaPosition = { lat: 14.5995, lng: 120.9842 };
-  var marker = new google.maps.Marker({
-    position: manilaPosition,
-    map: map,
-    title: 'Manila'
   });
-
+  infoWindow = new google.maps.InfoWindow();
+  showStoresMarkers();
 }
 
+
+
+
+// var manilaPosition = { lat: 14.5995, lng: 120.9842 };
+// var marker = new google.maps.Marker({
+//   position: manilaPosition,
+//   map: map,
+//   title: 'Manila'
+// });
+
+
+
+
+function displayStores() {
+  var storesHTML = '';
+  for (var [index, store] of stores.entries()) {
+    var address = store['addressLines'];
+    var phone = store['phoneNumber'];
+    storesHTML += `
+        <div class="store-container">
+            <div class="store-info-container">
+              <div class="store-address">
+                <span>${address[1]}</span>
+                <span>${address[0]}</span>
+              </div>
+              <div class="store-phone-number">
+                ${phone}
+              </div>
+            </div>
+            <div class="store-number-container">
+              <div class="store-number">
+                ${++index}
+              </div>
+            </div>
+          </div>
+    `
+    document.querySelector('.store-list').innerHTML = storesHTML;
+  }
+}
+
+
+function showStoresMarkers() {
+  var bounds = new google.maps.LatLngBounds();
+  for (var [index, store] of stores.entries()) {
+    var latlng = new google.maps.LatLng(
+      store["coordinates"]["latitude"],
+      store["coordinates"]["longitude"]
+    );
+    var storeOpen = store["openStatusText"];
+    var name = store['name'];
+    var address = store['addressLines'][0];
+    var phone = store['phoneNumber'];
+    bounds.extend(latlng);
+
+    createMarker(latlng, name, address, index + 1, storeOpen, phone)
+  }
+  map.fitBounds(bounds);
+}
+
+function createMarker(latlng, name, address, index, storeOpen, phone) {
+  var html = '<div class="marker-info-container">' +
+    '<span>' + name + '</span><br/>'
+    + '<div class="marker-open-container">' + storeOpen + '</div>'
+    + '<div class="marker-address-container">'
+    + '<div class="marker-icon">'
+    + '<span><i class="fa fa-location-arrow" aria-hidden="true"></i></span></div>'
+    + '<div class="marker">' + address + '</div></div>'
+    + '<div class="marker-icon">'
+    + '<span><i class="fa fa-phone fa-rotate-90" aria-hidden="true"></i></span></div>'
+    + '<div class="marker">' + phone + '</div></div></div>';
+  var marker = new google.maps.Marker({
+    map: map,
+    position: latlng,
+    label: String(index)
+  });
+  google.maps.event.addListener(marker, 'click', function () {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+  });
+  markers.push(marker);
+}
--- todo.md
+++ todo.md
@@ -1,4 +1,4 @@
-# Plan Of Action
+Day 1 # Plan Of Action
 
 - Create Wireframe DONE
 
@@ -13,3 +13,17 @@
 - Add Store List Container DONE
 
 - Add individual Store Container
+
+Day 2 # Plan of Action
+
+- Style the individual store item in the stores list DONE
+
+- Show all the stores in the stores list based on real word data DONE
+
+- Show the marker based on real world store data DONE
+
+- Show the info window DONE
+
+- MAYBE: Show the info windows when you click on the individual store
+
+- MAYBE: Add a beautiful transition on the hover of the individual store
